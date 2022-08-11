d4e0c70b114bd205eb27ad73eaac867ad75acf6c
murun3xi <gokhan.sevinc@gmail.com>
2022-08-11 12:08:01 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <?php
 echo 'deneme';
 
-$servername = "mysql://b37f567b3f741c:f43585bf@eu-cdbr-west-03.cleardb.net/heroku_123bc0464991a9f?reconnect=true";
-$username = "b37f567b3f741c";
-$password = "f43585bf";
+$servername = "eu-cdbr-west-03.cleardb.net";
+$username = "b10a4e0dc9cace";
+$password = "375fc6cd";
+$dbname = "heroku_06c217a53ac4242";
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname="heroku_123bc0464991a9f", $username, $password);
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname, $username, $password");
   //$conn = new PDO("mysql://b37f567b3f741c:f43585bf@eu-cdbr-west-03.cleardb.net/heroku_123bc0464991a9f?reconnect=true");
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
