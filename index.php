72414ca6ca3f2a5c2ba4b056054dfd14962ddda4
murun3xi <gokhan.sevinc@gmail.com>
2022-08-11 11:38:16 +0300

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ try {
   //$conn = new PDO("mysql://b37f567b3f741c:f43585bf@eu-cdbr-west-03.cleardb.net/heroku_123bc0464991a9f?reconnect=true");
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
- // echo "Connected successfully";
+ echo "Connected successfully";
 } catch(PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
 }
