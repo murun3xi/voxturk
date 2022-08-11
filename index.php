bb4b440f92871259aff698d8af269bbc1e35a450
murun3xi <gokhan.sevinc@gmail.com>
2022-08-11 12:14:37 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $password = "375fc6cd";
 $dbname = "heroku_06c217a53ac4242";
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=$dbname, $username, $password");
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
   //$conn = new PDO("mysql://b37f567b3f741c:f43585bf@eu-cdbr-west-03.cleardb.net/heroku_123bc0464991a9f?reconnect=true");
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
